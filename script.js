a1ae4bfbb85f77327c67b45eec58f1ff88eb97ed
Abhishek Dolai <itsabhi183@gmail.com>
2023-05-26 17:44:45 +0530

    add modal box

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const overlay = document.querySelector(".overlay");
-const modelBox = document.querySelector(".modal-box");
+const modalBox = document.querySelector(".modal-box");
+const modelClose = document.querySelector(".js-model-btn");
+const signForm = document.querySelector(".js-signup-form");
+const mail = document.querySelector(".mail");
 const inputArea = document.querySelector(".search input");
 
-inputArea.addEventListener("click", function (e) {
-  document.querySelector(".suggest-area").classList.remove("hidden");
-});
-
-// document.addEventListener("click", function (e) {
-//   if (!e.target.classList.contains("suggest-list") || e.target !== inputArea) {
-//     console.log(e.target);
-//     console.log("closed");
-//     document.querySelector(".suggest-area").classList.add("hidden");
-//   }
-// });
-
-modelBtn.addEventListener("click", function () {
-  overlay.classList.remove("hidden");
-  modelBox.classList.remove("hidden");
-});
-
-overlay.addEventListener("click", function () {
+function closeModal() {
   overlay.classList.add("hidden");
-  modelBox.classList.add("hidden");
+  modalBox.classList.add("hidden");
+}
+
+signForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  mail.textContent = document.querySelector(".input-email").value;
+  document.querySelector(".input-email").value = "";
+  overlay.classList.remove("hidden");
+  modalBox.classList.remove("hidden");
 });
+
+overlay.addEventListener("click", closeModal);
+modelClose.addEventListener("click", closeModal);
