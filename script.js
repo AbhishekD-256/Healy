e0c92d90179c8c7ebdd25abb4d1f74dbc266995e
Abhishek Dolai <itsabhi183@gmail.com>
2023-05-29 12:57:02 +0530

    add slider component

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const modalBox = document.querySelector(".modal-box");
 const modelClose = document.querySelector(".js-model-btn");
 const signForm = document.querySelector(".js-signup-form");
 const mail = document.querySelector(".mail");
-const inputArea = document.querySelector(".search input");
+const inputAreas = document.querySelectorAll(".search input");
+const suggestList = document.querySelectorAll(".suggest-list");
+const leftBtn = document.querySelector(".js-left-btn");
+const rightBtn = document.querySelector(".js-right-btn");
+const articlesList = document.querySelector(".health-article");
+const articleCards = document.querySelectorAll(".health-article-card");
 
 //form prevent
 formSearch.addEventListener("submit", function (e) {
   e.preventDefault();
 });
+
 // open modal-box
 
 signForm.addEventListener("submit", function (e) {
@@ -31,3 +37,44 @@ function closeModal() {
 
 overlay.addEventListener("click", closeModal);
 modelClose.addEventListener("click", closeModal);
+
+//
+
+inputAreas.forEach((el) =>
+  el.addEventListener("click", function () {
+    console.log(el);
+    suggestList.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+  })
+);
+
+// slider component
+
+const cardWidth = articleCards[0].offsetWidth;
+let currentPosition = 0;
+
+// leftBtn.addEventListener("click", slideLeft);
+// rightBtn.addEventListener("click", slideRight);
+
+leftBtn.addEventListener("click", function () {
+  if (currentPosition > 0) {
+    currentPosition--;
+    moveCards();
+  }
+});
+
+rightBtn.addEventListener("click", function () {
+  if (currentPosition < articleCards.length - 1) {
+    currentPosition++;
+    moveCards();
+  }
+});
+
+function moveCards() {
+  for (let i = 0; i < articleCards.length; i++) {
+    articleCards[i].style.transform = `translateX(-${
+      currentPosition * cardWidth
+    }px)`;
+  }
+}
