167cd9adf3cb542fa42a4f45479795a82cbb62a9
Abhishek Dolai <itsabhi183@gmail.com>
2023-05-29 13:16:18 +0530

    modify cardWiidth

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const formSearch = document.querySelector(".search");
+const suggestList = document.querySelectorAll(".suggest-list");
+const inputArea = document.querySelector(".js-area");
+const inputHospital = document.querySelector(".js-hospital");
+const inputInsurance = document.querySelector(".js-insurance");
+
 const overlay = document.querySelector(".overlay");
 const modalBox = document.querySelector(".modal-box");
 const modelClose = document.querySelector(".js-model-btn");
 const signForm = document.querySelector(".js-signup-form");
 const mail = document.querySelector(".mail");
-const inputAreas = document.querySelectorAll(".search input");
-const suggestList = document.querySelectorAll(".suggest-list");
+
 const leftBtn = document.querySelector(".js-left-btn");
 const rightBtn = document.querySelector(".js-right-btn");
 const articlesList = document.querySelector(".health-article");
 const articleCards = document.querySelectorAll(".health-article-card");
 
 //form prevent
+
 formSearch.addEventListener("submit", function (e) {
   e.preventDefault();
 });
+
+// search suggetions
 
 // open modal-box
 
@@ -38,24 +45,10 @@ function closeModal() {
 overlay.addEventListener("click", closeModal);
 modelClose.addEventListener("click", closeModal);
 
-//
-
-inputAreas.forEach((el) =>
-  el.addEventListener("click", function () {
-    console.log(el);
-    suggestList.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
-  })
-);
-
 // slider component
 
-const cardWidth = articleCards[0].offsetWidth;
+const cardWidth = 300;
 let currentPosition = 0;
-
-// leftBtn.addEventListener("click", slideLeft);
-// rightBtn.addEventListener("click", slideRight);
 
 leftBtn.addEventListener("click", function () {
   if (currentPosition > 0) {
@@ -65,7 +58,7 @@ leftBtn.addEventListener("click", function () {
 });
 
 rightBtn.addEventListener("click", function () {
-  if (currentPosition < articleCards.length - 1) {
+  if (currentPosition < articleCards.length) {
     currentPosition++;
     moveCards();
   }
